a103e2c5d2bf6671d84251f3525dc0549d068759
Denis Ryzhakov <denis.r@mail.ru>
2023-03-31 23:46:52 +0500

    First commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        TicTacToe board = new TicTacToe();
+        board.getField();
+        System.out.println(board.makeMove(1,1));
+        board.getField();
+        System.out.println(board.makeMove(1,1));
+        System.out.println(board.makeMove(1,2));
+        board.getField();
+        System.out.println(board.makeMove(2,1));
+        System.out.println(board.makeMove(2,2));
+        System.out.println(board.makeMove(3,1));
+        System.out.println(board.makeMove(3,1));
+        board.checkGame();
+        board.getField();
+        System.out.println();
+        board.newGame();
+        board.getField();
+        System.out.println(board.makeMove(2,2));
+        System.out.println(board.makeMove(2,2));
+        System.out.println(board.makeMove(2,3));
+        System.out.println(board.makeMove(1,2));
+        System.out.println(board.makeMove(3,2));
+        System.out.println(board.makeMove(3,1));
+        System.out.println(board.makeMove(1,3));
+        System.out.println(board.makeMove(3,3));
+        System.out.println(board.makeMove(1, 1));
+        System.out.println(board.makeMove(2,1));
+        System.out.println(board.makeMove(3,3));
+        board.getField();
+        board.checkGame();
+    }
+}
